1c76748ce0e9e3e03acd86568eaac7d277117f90
fabiolucassilva <98435308+fabiolucassilva@users.noreply.github.com>
2024-05-28 20:09:16 -0300

    Base Trabalho

---
+++ src/test/java/com/uff_carona/carona/CaronaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uff_carona.carona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaronaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
